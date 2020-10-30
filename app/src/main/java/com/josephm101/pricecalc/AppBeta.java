63f465f9f38d42f9cc4de15de5e72b7a6243178c
Joseph Mignone <jmig3066@gmail.com>
2020-10-30 10:57:13 -0400

    Added Preferences.java, and started adding preference & settings functionality

--- app/src/main/java/com/josephm101/pricecalc/AppBeta.java
+++ app/src/main/java/com/josephm101/pricecalc/AppBeta.java
@@ -9,28 +9,31 @@ public class AppBeta {
 
     public static void ShowBetaMessage(final Context context)
     {
-        SharedPreferences settings = context.getApplicationContext().getSharedPreferences(Preferences.BETA_MODE_MSG_PREF, 0);
-        int homeScore = settings.getInt("homeScore", 0);
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(context)
-                .setTitle("Beta Mode")
-                .setMessage("This app is still currently in beta, and is (at the moment) intended for testing purposes only. As a result, issues such as instability, inconsistencies, and occasional bugs will be present throughout the application. \r\nAlso, any data such as settings and lists will be lost when the app is closed.\r\n \r\nYou have been warned.")
-                .setIcon(R.drawable.ic_baseline_warning_24)
-                .setPositiveButton("OK, Continue", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        dialog.dismiss();
-                    }
-                })
-                .setNegativeButton("Continue, and don't show this again", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                        SharedPreferences settings = context.getApplicationContext().getSharedPreferences(Preferences.BETA_MODE_MSG_PREF, 0);
-                        SharedPreferences.Editor editor = settings.edit();
-                        editor.putInt("doNotShowAgain", 1);
-                        editor.apply();
-                    }
-                })
-                .setCancelable(true);
-        alertDialog.show();
+        SharedPreferences settings = context.getApplicationContext().getSharedPreferences(Preferences.BetaMode.PreferenceGroup, 0);
+        int showMessage = settings.getInt(Preferences.BetaMode.ENTRY_BETA_MODE_MSG_PREF, 0);
+        if(showMessage != 1) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(context)
+                    .setTitle("Beta Mode")
+                    .setMessage("This app is still currently in beta, and is (at the moment) intended for testing purposes only. As a result, issues such as instability, inconsistencies, and occasional bugs will be present throughout the application. \r\nAlso, any data such as settings and lists will be lost when the app is closed.\r\n \r\nYou have been warned.")
+                    .setIcon(R.drawable.ic_baseline_warning_24)
+                    .setPositiveButton("OK, Continue", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            dialog.dismiss();
+                        }
+                    })
+                    .setNegativeButton("Continue, and don't show this again", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            SharedPreferences settings = context.getApplicationContext().getSharedPreferences(Preferences.BetaMode.PreferenceGroup, 0);
+                            SharedPreferences.Editor editor = settings.edit();
+                            editor.putInt(Preferences.BetaMode.ENTRY_BETA_MODE_MSG_PREF, 1);
+                            editor.apply();
+                            dialog.dismiss();
+                        }
+                    })
+                    .setCancelable(true);
+            alertDialog.show();
+        }
     }
 }
--- app/src/main/java/com/josephm101/pricecalc/Preferences.java
+++ app/src/main/java/com/josephm101/pricecalc/Preferences.java
@@ -1,5 +1,14 @@
 package com.josephm101.pricecalc;
 
 public class Preferences {
-    public static String BETA_MODE_MSG_PREF = "BETA_MODE_MESSAGE";
+    public static class BetaMode
+    {
+        public static String PreferenceGroup = "BETA_MODE_MESSAGE";
+        public static String ENTRY_BETA_MODE_MSG_PREF = "doNotShowAgain";
+    }
+
+    public static void SetPreference(String preferenceString, int value)
+    {
+
+    }
 }
