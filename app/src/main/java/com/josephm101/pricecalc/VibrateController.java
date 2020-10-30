77c6e2a395aaf11ae6408a2cec7e6e229e0d5246
Joseph Mignone <jmig3066@gmail.com>
2020-10-30 09:49:56 -0400

    Added VibrateController.java

---
+++ app/src/main/java/com/josephm101/pricecalc/VibrateController.java
@@ -0,0 +1,53 @@
+package com.josephm101.pricecalc;
+
+import android.content.Context;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+
+public class VibrateController {
+    Vibrator vibrator;
+    int tapDuration = 200;
+    int shortVibrate = 300;
+    int pauseVibrate = 100;
+    int longVibrate = 500;
+
+    public enum VibrateEffect
+    {
+        TAP,
+        DOUBLE_TAP,
+        NOTIFICATION_PATTERN
+    }
+
+    public VibrateController(Context context) {
+        vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+    }
+
+    public void TapEffect() {
+        vibrator.vibrate(tapDuration);
+    }
+
+    public void doVibrate(int duration) {
+        vibrator.vibrate(duration);
+    }
+
+    /*
+    public void doVibrate(VibrateEffect vibrateEffect) {
+        switch (vibrateEffect)
+        {
+            case TAP:
+                doVibrate(tapDuration);
+                break;
+            case DOUBLE_TAP:
+                doVibrate(shortVibrate);
+                wait(pauseVibrate);
+        }
+        vibrator.vibrate(vibrateEffect);
+    }
+    */
+
+    private void HandleVibrations(int duration) {
+        if (vibrator.hasVibrator()) {
+            vibrator.vibrate(duration);
+        }
+    }
+}
