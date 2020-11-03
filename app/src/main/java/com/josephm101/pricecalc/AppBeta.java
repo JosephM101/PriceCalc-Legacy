48b195d5ff56c1602aabe8fb9a3ee576e1832a5c
Joseph Mignone <jmig3066@gmail.com>
2020-11-03 10:53:36 -0500

    Updated "AppBeta" message, and changed version to "1.0alpha2".

--- app/src/main/java/com/josephm101/pricecalc/AppBeta.java
+++ app/src/main/java/com/josephm101/pricecalc/AppBeta.java
@@ -6,14 +6,13 @@ import android.content.DialogInterface;
 import android.content.SharedPreferences;
 
 public class AppBeta {
-
     public static void ShowBetaMessage(final Context context) {
         SharedPreferences settings = context.getApplicationContext().getSharedPreferences(Preferences.BetaMode.PreferenceGroup, 0);
         int showMessage = settings.getInt(Preferences.BetaMode.ENTRY_BETA_MODE_MSG_PREF, 0);
         if (showMessage != 1) {
             AlertDialog.Builder alertDialog = new AlertDialog.Builder(context)
                     .setTitle("Beta Mode")
-                    .setMessage("This app is still currently in beta, and is (at the moment) intended for testing purposes only. As a result, issues such as instability, inconsistencies, and occasional bugs will be present throughout the application.\r\n \r\nYou have been warned.")
+                    .setMessage("This app is still currently in beta. As a result, issues such as instability, inconsistencies, and occasional bugs will be present throughout the application.\r\n \r\nYou have been warned.")
                     .setIcon(R.drawable.ic_baseline_warning_24)
                     .setPositiveButton("OK, Continue", new DialogInterface.OnClickListener() {
                         @Override
@@ -21,7 +20,7 @@ public class AppBeta {
                             dialog.dismiss();
                         }
                     })
-                    .setNegativeButton("Continue, and don't show this again", new DialogInterface.OnClickListener() {
+                    .setNegativeButton("Continue, and don't show again", new DialogInterface.OnClickListener() {
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
                             SharedPreferences settings = context.getApplicationContext().getSharedPreferences(Preferences.BetaMode.PreferenceGroup, 0);
