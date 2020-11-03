e89141cf86096ccb625c34f8a6326124d19b6952
Joseph Mignone <jmig3066@gmail.com>
2020-11-03 10:48:28 -0500

    Fixed a bug where the text color of the list items didn't match that of the app theme.

--- app/src/main/java/com/josephm101/pricecalc/CustomAdapter.java
+++ app/src/main/java/com/josephm101/pricecalc/CustomAdapter.java
@@ -55,27 +55,21 @@ public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnCli
     public View getView(int position, View convertView, ViewGroup parent) {
         // Get the data item for this position
         DataModel dataModel = getItem(position);
+
         // Check if an existing view is being reused, otherwise inflate the view
-        ViewHolder viewHolder; // view lookup cache stored in tag
+        ViewHolder viewHolder = null; // view lookup cache stored in tag
         final View result;
 
         if (convertView == null) {
-            viewHolder = new ViewHolder();
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext().getApplicationContext());
+            //LayoutInflater inflater = LayoutInflater.from(parent.getContext().getApplicationContext());
+            LayoutInflater inflater = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE); //Only method that supported app theme matching
             convertView = inflater.inflate(R.layout.row_item, parent, false);
-/*
-            viewHolder.txtName = (TextView) convertView.findViewById(R.id.itemName);
-            viewHolder.txtPrice = (TextView) convertView.findViewById(R.id.itemCost);
-            viewHolder.txtDeductible = (TextView) convertView.findViewById(R.id.isTaxable);
-*/
-
+            viewHolder = new ViewHolder();
             viewHolder.txtName = convertView.findViewById(R.id.itemName);
             viewHolder.txtPrice = convertView.findViewById(R.id.itemCost);
             viewHolder.txtDeductible = convertView.findViewById(R.id.isTaxable);
             //viewHolder.txtTotal = (TextView) convertView.findViewById(R.id.itemCostTotal);
-
             result = convertView;
-
             convertView.setTag(viewHolder);
         } else {
             viewHolder = (ViewHolder) convertView.getTag();
@@ -85,17 +79,7 @@ public class CustomAdapter extends ArrayAdapter<DataModel> implements View.OnCli
         Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.up_from_bottom);
         result.startAnimation(animation);
         lastPosition = position;
-
         viewHolder.txtName.setText(dataModel.getItemName());
-/*
-        StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append("$");
-        stringBuilder.append(dataModel.getItemPrice());
-        stringBuilder.append(" (Quantity: ");
-        stringBuilder.append(dataModel.getItemQuantity());
-        stringBuilder.append(")");
-        viewHolder.txtPrice.setText(stringBuilder.toString());
-*/
         String stringBuilder = "$" +
                 dataModel.getItemPrice() +
                 " (Quantity: " +
