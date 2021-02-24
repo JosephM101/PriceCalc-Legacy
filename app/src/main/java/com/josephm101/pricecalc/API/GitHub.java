7626ec29e71749571484cfebb6dddeb0befe1cf7
Joseph Mignone <jmig3066@gmail.com>
2021-02-24 08:39:23 -0500

    Finished first revision of GitHub API communication library

--- app/src/main/java/com/josephm101/pricecalc/API/GitHub.java
+++ app/src/main/java/com/josephm101/pricecalc/API/GitHub.java
@@ -11,69 +11,109 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import java.util.Date;
 import java.util.TimeZone;
 
 public class GitHub {
-    String userName;
-    String repoName;
+
+/*    public static class GitHubUpdateListener implements RetrievalListener {
+        @Override
+        public void onRetrievalComplete(ReleaseInfo releaseInfo) {
+
+        }
+
+        @Override
+        public void onRetrievalError(String request) {
+
+        }
+    }*/
+
+
+    String userName; //Required for link generation.
+    String repoName; //Required for link generation.
     String apiCall_prefix = "https://api.github.com/repos/";
     String apiCall_suffix = "releases/latest";
     String apiCall_url_separator = "/";
     private JsonElement response;
 
-    public GitHub(String user_name, String repo_name) throws IOException {
-        userName = user_name;
-        repoName = repo_name;
-        //Make the API call.
-        ReleaseInfo rInfo = GetData();
-        Log.d("GIT1", rInfo.getReleaseName());
-        Log.d("GIT2", rInfo.getDownloadUrl());
+    public interface RetrievalListener {
+        void onRetrievalComplete(ReleaseInfo releaseInfo);
+        void onRetrievalError(String request);
     }
 
-    public ReleaseInfo GetData() throws IOException {
-        response = GetJsonResponse(generateApiCallURL());
-        JsonObject root = response.getAsJsonObject();
-        if (!(root.isJsonNull())) {
-            ReleaseInfo releaseInfo = new ReleaseInfo();
-            releaseInfo.setDownloadUrl(root.getAsJsonArray("assets").get(0).getAsJsonObject().get("browser_download_url").getAsString());
-            releaseInfo.setReleaseNotes(root.get("body").getAsString());
-            releaseInfo.setReleaseName(root.get("name").getAsString());
-            releaseInfo.setReleaseVersion(root.get("tag_name").getAsString());
-            String utcString = root.get("created_at").getAsString();
-            releaseInfo.setUploadDate_raw(utcString);
-            Log.d("UTC", utcString);
-            Instant timestamp = Instant.parse(utcString);
-            ZonedDateTime currentTime = timestamp.atZone(TimeZone.getDefault().toZoneId());
-            Date finalDate = Date.from(currentTime.toInstant());
-            Log.d("Local", currentTime.toString());
-            releaseInfo.setUploadDate(finalDate);
-            releaseInfo.setUploadDateString(currentTime.toString());
-            return releaseInfo;
-        } else {
-            return null;
-        }
+    private RetrievalListener listener;
+
+    public GitHub(String user_name, String repo_name, RetrievalListener listener) {
+        userName = user_name; //Required for link generation
+        repoName = repo_name; //Required for link generation
+        this.listener = listener;
     }
 
-    private JsonElement GetJsonResponse(String url_to_request_from) throws IOException {
+    public GitHub(String user_name, String repo_name) {
+        userName = user_name; //Required for link generation
+        repoName = repo_name; //Required for link generation
+        this.listener = null;
+    }
+
+    public void GetData(){
+        Thread thread = new Thread(() -> {
+            try {
+                response = GetJsonResponse(generateApiCallURL());
+                JsonObject root = response.getAsJsonObject();
+                if (!(root.isJsonNull())) { //If it's not null, we can keep going.
+                    ReleaseInfo releaseInfo = new ReleaseInfo();
+                    releaseInfo.setDownloadUrl(root.getAsJsonArray("assets").get(0).getAsJsonObject().get("browser_download_url").getAsString());
+                    releaseInfo.setReleaseNotes(root.get("body").getAsString());
+                    releaseInfo.setReleaseName(root.get("name").getAsString());
+                    releaseInfo.setReleaseVersion(root.get("tag_name").getAsString());
+                    String utcString = root.get("created_at").getAsString();
+                    releaseInfo.setUploadDate_raw(utcString);
+                    Log.d("UTC", utcString);
+                    Instant timestamp = Instant.parse(utcString);
+                    ZonedDateTime currentTime = timestamp.atZone(TimeZone.getDefault().toZoneId());
+                    Date finalDate = Date.from(currentTime.toInstant());
+                    Log.d("Local", currentTime.toString());
+                    releaseInfo.setUploadDate(finalDate);
+                    releaseInfo.setUploadDateString(currentTime.toString());
+                    Log.d("GIT1", releaseInfo.getReleaseName());
+                    Log.d("GIT2", releaseInfo.getDownloadUrl());
+                    if (listener != null)
+                        listener.onRetrievalComplete(releaseInfo); //Everything worked. Return the final class to the listener.
+                } else {
+                    if (listener != null)
+                        listener.onRetrievalError(generateApiCallURL()); //Something happened; issue an error
+                }
+            }
+            catch (Exception ex) //We ran into an exception; issue an error, and throw the information out.
+            {
+                if (listener != null)
+                    listener.onRetrievalError(generateApiCallURL());
+                ex.printStackTrace();
+            }
+        });
+        thread.start();
+    }
+
+    private JsonElement GetJsonResponse(String url_to_request_from) throws IOException { //Gets JSON response from the URL we generated.
         URL url = new URL(url_to_request_from);
         URLConnection request = url.openConnection();
         request.connect();
         return JsonParser.parseReader(new InputStreamReader((InputStream) request.getContent()));
     }
 
-    private String generateApiCallURL() {
+    private String generateApiCallURL() { //Generates request URL from the user's GitHub username and the repository name. Example: https://api.github.com/repos/JosephM101/PriceCalc/releases/latest
         return apiCall_prefix +
                 userName +
                 apiCall_url_separator +
                 repoName +
                 apiCall_url_separator +
                 apiCall_suffix;
+    }
+
+    public GitHub setGitHubUpdateListener(RetrievalListener listener) {
+        this.listener = listener;
+        return this;
     }
 }
