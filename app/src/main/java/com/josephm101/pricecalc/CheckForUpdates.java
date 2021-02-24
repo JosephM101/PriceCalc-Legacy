f3fde92a7a89c3b5adb0c16d5bcdf01945569e1a
Joseph Mignone <jmig3066@gmail.com>
2021-02-23 19:34:53 -0500

    Code updates

--- app/src/main/java/com/josephm101/pricecalc/CheckForUpdates.java
+++ app/src/main/java/com/josephm101/pricecalc/CheckForUpdates.java
@@ -3,8 +3,19 @@ package com.josephm101.pricecalc;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.app.DownloadManager;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInstaller;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.MemoryFile;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.github.javiersantos.appupdater.AppUpdaterUtils;
@@ -14,7 +25,17 @@ import com.github.javiersantos.appupdater.objects.Update;
 
 import org.w3c.dom.Text;
 
+import java.io.BufferedInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.net.URLConnection;
+
 public class CheckForUpdates extends AppCompatActivity {
+    public static final int progress_bar_type = 0;
+    private ProgressDialog pDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +45,12 @@ public class CheckForUpdates extends AppCompatActivity {
         CardView loading_CardView = findViewById(R.id.cardView_checkingForUpdates);
         CardView cardView_updateInfo = findViewById(R.id.cardView_updateInfo);
         CardView cardView_noUpdates = findViewById(R.id.cardView_noUpdates);
+        CardView cardView_updateError = findViewById(R.id.cardView_updateError);
 
         loading_CardView.setVisibility(View.VISIBLE);
         cardView_updateInfo.setVisibility(View.GONE);
         cardView_noUpdates.setVisibility(View.GONE);
+        cardView_updateError.setVisibility(View.GONE);
 
         /**
          * Initialize the updater
@@ -56,18 +79,122 @@ public class CheckForUpdates extends AppCompatActivity {
                     currentVersionTextView.setText(BuildConfig.VERSION_NAME);
                     newVersionTextView.setText(update.getLatestVersion());
                     changelogTextView.setText(update.getReleaseNotes());
+                    Button button_updateNow = findViewById(R.id.button_confirmUpdate);
+                    button_updateNow.setOnClickListener(v -> {
+                        //DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+                        //Uri update_link = Uri.parse(update.getUrlToDownload().toString());
+                        //DownloadManager.Request request = new DownloadManager.Request(update_link);
+                        //request.setTitle("PriceCalc");
+                        //request.setDescription("Downloading update for PriceCalc...");
+                        //request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+                        //request.setVisibleInDownloadsUi(false);
+                        //request.setDestinationUri(Uri.parse("file://" + getApplicationContext().getFilesDir() + "/pricecalc_update.apk"));
+                        pDialog = new ProgressDialog(getApplicationContext());
+                        pDialog.setMessage("Downloading update...");
+                        pDialog.setIndeterminate(false);
+                        pDialog.setMax(100);
+                        pDialog.setProgressStyle(ProgressDialog.STYLE_HORIZONTAL);
+                        pDialog.setCancelable(false);
+                        pDialog.show();
+                    });
+                    Button button_dismiss = findViewById(R.id.button_updateLater);
+                    button_dismiss.setOnClickListener(v -> finish());
                     cardView_updateInfo.setVisibility(View.VISIBLE);
                 } else {
                     setTitle("No new updates.");
                     cardView_noUpdates.setVisibility(View.VISIBLE);
+                    Button button_dismiss = findViewById(R.id.button_dismissUpdateDialog);
+                    button_dismiss.setOnClickListener(v -> finish());
                 }
             }
 
             @Override
             public void onFailed(AppUpdaterError error) {
-
+                loading_CardView.setVisibility(View.GONE);
+                setTitle("Couldn't update.");
+                cardView_updateError.setVisibility(View.VISIBLE);
+                Button button_dismiss = findViewById(R.id.button_dismissFailedDialog);
+                button_dismiss.setOnClickListener(v -> finish());
             }
         });
         appUpdater.start();
+    }
+
+    class DownloadFileFromURL extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        /**
+         * Downloading file in background thread
+         * */
+        @Override
+        protected String doInBackground(String... f_url) {
+            int count;
+            try {
+                URL url = new URL(f_url[0]);
+                URLConnection connection = url.openConnection();
+                connection.connect();
+
+                // this will be useful so that you can show a typical 0-100%
+                // progress bar
+                int lengthOfFile = connection.getContentLength();
+
+                // download the file
+                InputStream input = new BufferedInputStream(url.openStream(),
+                        8192);
+
+                // Output stream
+                OutputStream output = new FileOutputStream("pricecalc_update.apk");
+
+                byte data[] = new byte[1024];
+
+                long total = 0;
+
+                while ((count = input.read(data)) != -1) {
+                    total += count;
+                    // publishing the progress....
+                    // After this onProgressUpdate will be called
+                    publishProgress("" + (int) ((total * 100) / lengthOfFile));
+
+                    // writing data to file
+                    output.write(data, 0, count);
+                }
+
+                // flushing output
+                output.flush();
+
+                // closing streams
+                output.close();
+                input.close();
+
+            } catch (Exception e) {
+                Log.e("Error: ", e.getMessage());
+            }
+
+            return null;
+        }
+
+        /**
+         * Updating progress bar
+         * */
+        protected void onProgressUpdate(String... progress) {
+            // setting progress percentage
+            pDialog.setProgress(Integer.parseInt(progress[0]));
+        }
+
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        @Override
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog after the file was downloaded
+            pDialog.dismiss();
+            Intent promptInstall = new Intent(Intent.ACTION_VIEW)
+                    .setDataAndType(Uri.parse("pricecalc_update.apk"),
+                            "application/vnd.android.package-archive");
+            startActivity(promptInstall);
+        }
     }
 }
